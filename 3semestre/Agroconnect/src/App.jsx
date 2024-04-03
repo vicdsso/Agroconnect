90b70b38197d32d9ef0186811fb9c65539c8d60e
vicdsso <vicdsso@gmail.com>
2024-04-03 11:47:39 -0300

    ok

--- 3semestre/Agroconnect/src/App.jsx
+++ 3semestre/Agroconnect/src/App.jsx
@@ -1,14 +1,17 @@
-import React from 'react'
-import './App.css'
-import Publicacao from './components/Publicacao/Publicacao'
+import React from "react";
+import "./App.css";
+import Produtos from "./components/Publicacoes/Produtos";
+import Maquinas from "./components/Publicacoes/Maquinas";
+import Servicos from "./components/Publicacoes/Servicos";
 
 function App() {
- 
   return (
     <>
-    <Publicacao></Publicacao>
+      <Maquinas></Maquinas>
+      <Servicos></Servicos>
+      <Produtos></Produtos>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- 3semestre/Agroconnect/src/components/Publicacoes/Maquinas.jsx
+++ 3semestre/Agroconnect/src/components/Publicacoes/Maquinas.jsx
@@ -1,88 +1,26 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import style from "./Publicacao.module.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 
+/*Ideia fazendeiro vendendo uma maquina*/
 const Publicacao = () => {
   return (
-    <>
-      <section>
+    <>Maquinas
+
+<section>
         <div className={style.estrutura}>
           <div className={style.row}>
             <div className={style.col}>
               <img
                 className={style["logo-parceiros"]}
-                src="https://blog4.mfrural.com.br/wp-content/uploads/2020/08/fazenda-venda3.jpg"                alt="Logo Parceiros"
-              />
-
-              <div className={style["post-textos"]}>
-                <p className={style["nome-parceiro"]}>Sol Poente</p>
-                <p className={style["tipo-parceiro"]}>
-                  Agricultor|Produto|
-                  <img
-                    className={style.pontinhos}
-                    src="/img/pontos.png"
-                    alt="pontinhos"
-                  />
-                </p>
-              </div>
-            </div>
-            <hr></hr>
-            <center>
-              <h5>Mel Puro e Natural</h5>
-            </center>
-
-            {}
-            <p className={style["texto-parceiros"]}>
-              Se você deseja adquirir nosso mel em barris de 100 kg para sua
-              marca, entre em contato conosco para discutir detalhes, preços e
-              prazos de entrega. Escolha nosso mel para elevar o sabor de
-              seus produtos! 100% puro e natural - Sem aditivos... ou
-              conservantes - Colhido e processado com carinho - Sabor
-              excepcional e versátil
-              {}
-            </p>
-            <p className={style["ler-mais"]}>Ler mais</p>
-
-            <div>
-              <img
-                className={style["imagem-parceiros"]}
-                src="https://th.bing.com/th/id/R.58daae556f7d140c3cbe3e2e843b28cf?rik=wcX2pClLsE8%2fyA&pid=ImgRaw&r=0" alt=""/>
-      
-            </div>
-
-            <div className={style["botoes-container"]}>
-              <img
-                className={style["curtirFavoritar"]}
-                src="/img/coracao.png"
-                alt="Curtir"
-              />
-              <button className={style["btn-saibamais"]} type="button">
-                Entrar na publicação
-              </button>
-              <img
-                className={style["curtirFavoritar"]}
-                src="/img/favorito.png"
-                alt="Favoritar"
-              />
-            </div>
-          </div>
-        </div>
-      </section>
-
-      <section>
-        <div className={style.estrutura}>
-          <div className={style.row}>
-            <div className={style.col}>
-              <img
-                className={style["logo-parceiros"]}
-                src=" https://revistarpanews.com.br/wp-content/uploads/2020/09/colhedora-de-cana.jpg"
+                src=""
                 alt="Logo Parceiros"
               />
-
               <div className={style["post-textos"]}>
-                <p className={style["nome-parceiro"]}>Fazenda Doce Açúcar</p>
+                <p className={style["nome-parceiro"]}>
+                    </p>
                 <p className={style["tipo-parceiro"]}>
-                  Agricultor|Produto|
+                  Fazenda|Maquina|
                   <img
                     className={style.pontinhos}
                     src="/img/pontos.png"
@@ -92,18 +30,14 @@ const Publicacao = () => {
               </div>
             </div>
             <hr></hr>
+
             <center>
-              <h5>Oferta de Safra de Cana-de-Açúcar</h5>
+              <h5></h5>
             </center>
 
             {}
             <p className={style["texto-parceiros"]}>
-              Temos o prazer de oferecer nossa safra de cana-de-açúcar de alta
-              qualidade diretamente da nossa Fazenda. Nossa plantação é
-              cultivada com cuidado e garantindo qualidade. Cada tonelada é
-              vendida a um preço justo de R$ 350... Contato: Entre em contato
-              conosco pelo e-mail: fazendadoceacucar@gmail.com ou pelo telefone:
-              (11) 1234-5678.
+              
               {}
             </p>
             <p className={style["ler-mais"]}>Ler mais</p>
@@ -111,7 +45,7 @@ const Publicacao = () => {
             <div>
               <img
                 className={style["imagem-parceiros"]}
-                src="https://blog4.mfrural.com.br/wp-content/uploads/2022/09/shutterstock_81418672-1090x660.jpg"
+                src=""
                 alt=""
               />
             </div>
@@ -122,6 +56,7 @@ const Publicacao = () => {
                 src="/img/coracao.png"
                 alt="Curtir"
               />
+
               <button className={style["btn-saibamais"]} type="button">
                 Entrar na publicação
               </button>
@@ -135,6 +70,69 @@ const Publicacao = () => {
         </div>
       </section>
 
+
+      <section>
+        <div className={style.estrutura}>
+          <div className={style.row}>
+            <div className={style.col}>
+              <img
+                className={style["logo-parceiros"]}
+                src=""
+                alt="Logo Parceiros"
+              />
+              <div className={style["post-textos"]}>
+                <p className={style["nome-parceiro"]}>
+                    </p>
+                <p className={style["tipo-parceiro"]}>
+                  Empresa|Maquina|
+                  <img
+                    className={style.pontinhos}
+                    src="/img/pontos.png"
+                    alt="pontinhos"
+                  />
+                </p>
+              </div>
+            </div>
+            <hr></hr>
+
+            <center>
+              <h5></h5>
+            </center>
+
+            {}
+            <p className={style["texto-parceiros"]}>
+              
+              {}
+            </p>
+            <p className={style["ler-mais"]}>Ler mais</p>
+
+            <div>
+              <img
+                className={style["imagem-parceiros"]}
+                src=""
+                alt=""
+              />
+            </div>
+
+            <div className={style["botoes-container"]}>
+              <img
+                className={style["curtirFavoritar"]}
+                src="/img/coracao.png"
+                alt="Curtir"
+              />
+
+              <button className={style["btn-saibamais"]} type="button">
+                Entrar na publicação
+              </button>
+              <img
+                className={style["curtirFavoritar"]}
+                src="/img/favorito.png"
+                alt="Favoritar"
+              />
+            </div>
+          </div>
+        </div>
+      </section>
       <section>
         <div className={style.estrutura}>
           <div className={style.row}>
