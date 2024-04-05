bf4f442d29915c3e7fd3b3230e41fc6ac9f37f48
vicdsso <vicdsso@gmail.com>
2024-04-05 11:47:51 -0300

    navBar kamy

--- 3semestre/Agroconnect/src/App.jsx
+++ 3semestre/Agroconnect/src/App.jsx
@@ -3,10 +3,11 @@ import "./App.css";
 import Produtos from "./components/Publicacoes/Produtos";
 import Maquinas from "./components/Publicacoes/Maquinas";
 import Servicos from "./components/Publicacoes/Servicos";
-
+import Home from "./components/Homepage/Home";
 function App() {
   return (
     <>
+    <Home></Home>
       <Maquinas></Maquinas>
       <Servicos></Servicos>
       <Produtos></Produtos>
